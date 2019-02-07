6775841026a95d132bd513fc712b1759c8f7c57e
santhosh921 <43337965+santhosh921@users.noreply.github.com>
2019-02-07 14:24:44 +0530

    Update print.c

---
+++ print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+   int n,i;
+     printf("Enter the number of time:");
+     scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+          printf("HELLO\n");
+     }
+
+}
+        
+    
