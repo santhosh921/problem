4fc5cbab8ce8b8c24bbbae14f36fc6e34da5d000
santhosh921 <43337965+santhosh921@users.noreply.github.com>
2019-02-26 11:54:16 +0530

    Update palindrom.c

--- palindrom.c
+++ palindrom.c
@@ -4,17 +4,24 @@ int main()
     int n, reversed = 0, rem, originalInteger;
     printf("Enter an integer: ");
     scanf("%d", &n);
-    originalInteger = n;
-    while( n!=0 )
+    if (n>1000)
     {
+        printf("no can't calculate");
+    }
+    else
+    {
+       originalInteger = n;
+       while( n!=0 )
+      {
         rem= n%10;
         reversed = reversed*10 + rem;
         n /= 10;
-    }
-    if (originalInteger == reversed)
+       }
+        if (originalInteger == reversed)
         printf("%d is a palindrome.", originalInteger);
-    else
+      else
         printf("%d is not a palindrome.", originalInteger);
+    }
     
     return 0;
 }
